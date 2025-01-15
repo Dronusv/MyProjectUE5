fca75c317f955763d0d237f2766f610caf27b022
Romchik <Dronusv22@mail.ru>
2025-01-15 22:39:43 +0300

    Sprint

---
+++ Source/LeaveMeAlone/Private/LevelActors/Pickup/LMAHealthPickup.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelActors/Pickup/LMAHealthPickup.h"
+
+// Sets default values
+ALMAHealthPickup::ALMAHealthPickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALMAHealthPickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALMAHealthPickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
